0b51be21761c6c654cbc5a64790883e47d6d173e
Shun Miyaki <shunmiyaki@hotmail.com>
2018-07-14 23:19:54 +0900

    First experiment Complete. NEXT: Test different shapes.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,41 @@
 /* global window */
 document.addEventListener('DOMContentLoaded', () => {
   //-----------------------------------------------------------------------------------------------
-  console.log('INITIAL CHECK');
-  // TITLE
-  // - Explanation.
+  // CODE FOR SECTION 'cups.svg'
+  const objectCups = document.querySelector("#objectCups");
+  objectCups.addEventListener("load", function() {
+    const svg = objectCups.contentDocument.querySelector("#cups");
+    const snap = Snap(svg);
+    const simpleCup = snap.select('#simple-cup');
+    const fancyCup = snap.select('#fancy-cup');
+    const simpleCupPoints = simpleCup.node.getAttribute('d');
+    const fancyCupPoints = fancyCup.node.getAttribute('d');
+    const toFancy = function(){
+      simpleCup.animate({ d: fancyCupPoints }, 1000, mina.backout, toSimple);
+    }
+    const toSimple = function(){
+      simpleCup.animate({ d: simpleCupPoints }, 1000, mina.backout, toFancy);
+    }
+    toSimple();
+   }, false);
+  //-----------------------------------------------------------------------------------------------
+  // CODE FOR SECTION '00_shapes.svg'
+  const objectShapes = document.querySelector("#objectShapes");
+  objectShapes.addEventListener("load", function() {
+    const svg = objectShapes.contentDocument.querySelector("#shapes");
+    const snap = Snap(svg);
+    debugger;
+    const simpleCup = snap.select('#simple-cup');
+    const fancyCup = snap.select('#fancy-cup');
+    const simpleCupPoints = simpleCup.node.getAttribute('d');
+    const fancyCupPoints = fancyCup.node.getAttribute('d');
+    const toFancy = function(){
+      simpleCup.animate({ d: fancyCupPoints }, 1000, mina.backout, toSimple);
+    }
+    const toSimple = function(){
+      simpleCup.animate({ d: simpleCupPoints }, 1000, mina.backout, toFancy);
+    }
+    toSimple();
+   }, false);
   //-----------------------------------------------------------------------------------------------
 });
